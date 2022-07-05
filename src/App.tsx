71849945468e550d5efdf4ba925d45d890c45e35
gurshant <gurshant53@gmail.com>
2022-07-04 21:37:43 -0700

    api requests complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
-import { SearchBar } from './components/SearchBar'
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Movie } from './requests'
 import './App.css';
 
 
@@ -9,11 +12,34 @@ import './App.css';
 
 function App() {
 
+  const [input, setInput] = React.useState('');
 
+  React.useEffect(() => {
+    //Runs on the first render
+    //And any time any dependency value changes
+    console.log(input)
+    Movie.one(input).then((r) =>
+      console.log(r)
+    ).catch((e) =>
+      console.log(e)
+    )
+  }, [input]);
   return (
     <Container maxWidth="sm" sx={{ marginTop: '10rem' }}>
       <CssBaseline />
-      <SearchBar />
+      <Box
+        component="form"
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="search"
+          label="Search For a Movie"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          fullWidth={true}
+        />
+      </Box>
     </Container>
   );
 }
--- src/requests.ts
+++ src/requests.ts
@@ -1,10 +1,8 @@
-const BASE_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=`;
+const BASE_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_OMDB_KEY}`;
 
 export const Movie = {
     one(title: string) {
-        return fetch(`${BASE_URL}/`, {
-            credentials: "include"
-        }).then(res => res.json());
+        return fetch(`${BASE_URL}&t=${title}`).then(res => res.json());
     }
 }
 
